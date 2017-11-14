6633d88e37ab12a9fa537f291d146fab4547a98f
floris09 <florismeininger@gmail.com>
2017-11-14 15:12:54 +0100

    show batches on homepage

--- src/containers/Lobby.js
+++ src/containers/Lobby.js
@@ -6,63 +6,21 @@ import fetchBatches, { fetchStudents } from '../actions/batches/fetch'
 import CreateBatchButton from '../components/batches/CreateBatchButton'
 import Paper from 'material-ui/Paper'
 import Menu from 'material-ui/Menu'
-import MenuItem from 'material-ui/MenuItem'
-import WatchBatchIcon from 'material-ui/svg-icons/image/remove-red-eye'
-import JoinBatchIcon from 'material-ui/svg-icons/social/person-add'
-import PlayBatchIcon from 'material-ui/svg-icons/hardware/videogame-asset'
-import WaitingIcon from 'material-ui/svg-icons/image/timelapse'
 import './Lobby.css'
 
 class Lobby extends PureComponent {
   componentWillMount() {
     this.props.fetchBatches()
-
-  }
-
-  goToBatch = batchId => event => this.props.push(`/play/${batchId}`)
-
-  isJoinable(batch) {
-    return batch.students.length < 2 &&
-      !this.isStudent(batch)
-  }
-
-  isStudent(batch) {
-    if (!this.props.currentUser) { return false }
-    return batch.students.map(p => p.userId)
-      .indexOf(this.props.currentUser._id) >= 0
-  }
-
-  isPlayable(batch) {
-    return this.isStudent(batch) && batch.students.length === 2
-  }
-
-  renderBatch = (batch, index) => {
-    let ActionIcon = this.isJoinable(batch) ? JoinBatchIcon : WatchBatchIcon
-    if (this.isStudent(batch)) ActionIcon = this.isPlayable(batch) ? PlayBatchIcon : WaitingIcon
-
-    if (!batch.students[0].name) { this.props.fetchStudents(batch) }
-
-    const title = batch.students.map(p => (p.name || null))
-      .filter(n => !!n)
-      .join(' vs ')
-
-    return (
-      <MenuItem
-        key={index}
-        onClick={this.goToBatch(batch._id)}
-        rightIcon={<ActionIcon />}
-        primaryText={title} />
-    )
   }
 
   render() {
     return (
       <div className="Lobby">
-        <h1>Lobby!</h1>
+        <h1>Batches</h1>
         <CreateBatchButton />
         <Paper className="paper">
           <Menu>
-            {this.props.batches.map(this.renderBatch)}
+            { this.props.batches.map((batch,index) => <h3 key={ index } > Batch #{ batch.batchNumber } </h3>) }
           </Menu>
         </Paper>
       </div>
