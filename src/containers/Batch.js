ec784674782f1aa5c8294e10ee43f47c023661b0
floris09 <florismeininger@gmail.com>
2017-11-15 08:56:42 +0100

    fix bug

--- src/containers/Batch.js
+++ src/containers/Batch.js
@@ -1,67 +1,33 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { fetchOneBatch, fetchStudents } from '../actions/batches/fetch'
-
-const studentShape = PropTypes.shape({
-  userId: PropTypes.string.isRequired,
-  pairs: PropTypes.arrayOf(PropTypes.string).isRequired,
-  name: PropTypes.string
-})
+import { fetchOneBatch } from '../actions/batches/fetch'
 
 class Batch extends PureComponent {
   static propTypes = {
     fetchOneBatch: PropTypes.func.isRequired,
-    fetchStudents: PropTypes.func.isRequired,
-    batch: PropTypes.shape({
-      _id: PropTypes.string.isRequired,
-      userId: PropTypes.string.isRequired,
-      students: PropTypes.arrayOf(studentShape),
-      draw: PropTypes.bool,
-      updatedAt: PropTypes.string.isRequired,
-      createdAt: PropTypes.string.isRequired,
-      started: PropTypes.bool,
-      turn: PropTypes.number.isRequired,
-      cards: PropTypes.arrayOf(PropTypes.shape({
-        symbol: PropTypes.string,
-        _id: PropTypes.string,
-        won: PropTypes.bool,
-        visible: PropTypes.bool
-      }))
-    }),
-    currentStudent: studentShape,
-    isStudent: PropTypes.bool,
-    isJoinable: PropTypes.bool,
-    hasTurn: PropTypes.bool
   }
 
-  componentWillMount() {
+  componentWillMount(){
+    const { pathname } = this.props.router.location
+    const id = pathname.slice(1)
 
+    this.props.fetchOneBatch(id)
   }
 
-  componentWillReceiveProps(nextProps) {
-    const { batch } = nextProps
-
-    if (batch && !batch.students[0].name) {
-      this.props.fetchStudents(batch)
-    }
-  }
 
   render() {
-    const { batch } = this.props
-
-    if (!batch) return null
 
     return (
       <div className="Batch">
-
-
+        <h3> Batch #{ this.props.batches.batchNumber }</h3>
       </div>
     )
   }
 }
 
-export default connect(null, {
-  fetchOneBatch,
-  fetchStudents
+const mapStateToProps = ({ batches, router }) => ({ batches, router })
+
+export default connect(mapStateToProps, {
+  fetchOneBatch
 })(Batch)
--- src/containers/Lobby.js
+++ src/containers/Lobby.js
@@ -3,8 +3,8 @@ import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import { push } from 'react-router-redux'
 import fetchBatches from '../actions/batches/fetch'
-import fetchStudents from '../actions/students/fetch'
 import CreateBatchButton from '../components/batches/CreateBatchButton'
+import { fetchOneBatch } from '../actions/batches/fetch'
 import Paper from 'material-ui/Paper'
 import Menu from 'material-ui/Menu'
 import './Lobby.css'
@@ -12,7 +12,11 @@ import './Lobby.css'
 class Lobby extends PureComponent {
   componentWillMount() {
     this.props.fetchBatches()
-    this.props.fetchStudents()
+  }
+
+  fetchBatch(id){
+    this.props.push(`/${id}`)
+    this.props.fetchOneBatch(id)
   }
 
   render() {
@@ -22,7 +26,7 @@ class Lobby extends PureComponent {
         <CreateBatchButton />
         <Paper className="paper">
           <Menu>
-            { this.props.batches.map((batch,index) => <h3 key={ index } > Batch #{ batch.batchNumber } </h3>) }
+            { this.props.batches.map((batch,index) => <h3 key={ index } onClick={ this.fetchBatch.bind(this, batch._id) } > Batch #{ batch.batchNumber } </h3>) }
           </Menu>
         </Paper>
       </div>
@@ -30,6 +34,6 @@ class Lobby extends PureComponent {
   }
 }
 
-const mapStateToProps = ({ batches, currentUser, students }) => ({ batches, currentUser, students })
+const mapStateToProps = ({ batches, currentUser }) => ({ batches, currentUser })
 
-export default connect(mapStateToProps, { fetchBatches, fetchStudents, push })(Lobby)
+export default connect(mapStateToProps, { fetchBatches, fetchOneBatch, push })(Lobby)
